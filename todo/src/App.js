38dac1353be994fbb59e9396b1fe6496ecb8d38b
sumana-hub <sumana.dhital@triosstudent.com>
2024-12-09 20:33:11 -0500

    Added Components

---
+++ todo/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Adam",
+      todoItems: [{ action: "Buy Flowers", done: false },
+      { action: "Get Shoes", done: false },
+      { action: "Collect Tickets", done: true },
+      { action: "Call Joe", done: false }
+      ]
+    }
+  }
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  }
+
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)) {
+      this.setState({
+        todoItems: [...this.state.todoItems, { action: task, done: false }]
+      });
+    }
+  }
+
+  toggleTodo = (todo) => this.setState({
+    todoItems: this.state.todoItems.map(item => item.action === todo.action
+      ? { ...item, done: !item.done } : item
+    )
+  });
+
+  todoTableRows = () => this.state.todoItems.map(item =>
+    <TodoRow key={item.action} item={item} callback={this.toggleTodo} />
+  );
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam"
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <TodoBanner name={this.state.userName} tasks={this.state.todoItems} />
+        <div className="container-fluid">
+          <TodoCreator callback={ this.createNewTodo} />
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Done</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.todoTableRows()}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
